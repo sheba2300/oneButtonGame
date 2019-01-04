e13ac7861ee9531b97db716c9a82a1537b12cfb0
sheba sow <ssow8171@bths,edu>
2019-01-04 10:12:48 -0500

    not done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,10 +2,15 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 
 public class Controller {
-    public void pressButton(ActionEvent event)
+    @FXML
+    private Button yellow;
+    public void pressButton()
     {
-        System.out.println("Hello World");
+        yellow.setOpacity(.5);
     }
 }
